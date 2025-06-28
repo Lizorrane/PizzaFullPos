b15fd57b4156d94d8cf4328505a7cd60ba37679f
lizorrane.mm <lizorrane.mm@AN0101709085936>
2025-06-28 08:32:01 -0300

    Adicionar arquivos de projeto.

---
+++ Pizza.API/Models/Pizza.cs
@@ -0,0 +1,12 @@
+﻿namespace Pizza.API.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Sabor { get; set; } = null!;
+        public int TempoPreparo { get; set; }
+        public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+        public decimal Preco {  get; set; }
+
+    }
+}
